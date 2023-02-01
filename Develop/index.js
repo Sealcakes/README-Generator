c7fcc13db938f65cfaad904d6cf647dbc06db23a
Sealcakes <taylorcseal@gmail.com>
2023-02-01 12:24:56 -0600

    creates readme questions, creates init function, creates function to create readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+
+let questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectTitle",
+    },
+
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "projectDescription",
+    },
+
+    {
+        type: "input",
+        message: "What are the project installation steps?",
+        name: "projectInstallation",
+    },
+
+    {
+        type: "input",
+        message: "What is the usage information for this project?",
+        name: "projectUsage",
+    },
+
+    {
+        type: "input",
+        message: "Who are the contributing authors of this project?",
+        name: "projectContributors",
+    },
+
+    {
+        type: "input",
+        message: "What are the testing instructions for this project?",
+        name: "projectTestInformation",
+    },
+
+    {
+        type: "list",
+        name: "License:",
+        message: "What license are you using for this project?",
+        choices: [
+            "Apache License 2.0",
+            "BSD 3-Clause License",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "Mozilla Public License 2.0",
+            "None"
+        ],
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const createReadMe = createFile => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./newReadme.md', createFile, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true
+            });
+        });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers) {
+
+        let createFile = generateMarkdown(answers);
+        console.log(typeof createFile);
+        createReadMe(createFile);
+    })
+}
 
 // Function call to initialize app
 init();
